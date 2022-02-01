3dd1cf1ed36260ded89162ea01f12a4b5234677b
Byrson Bisuna <bbisuna@ucsd.edu>
2022-02-01 08:50:08 -0800

    added python the hardway exercises

---
+++ books/hardway1/ch2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignore by python.
+
+print("I could have code like this.") # and the comment is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run"
+
+print("This will run.")
+
+
---
+++ books/hardway1/ch3.py
@@ -0,0 +1,8 @@
+print("I will now count my chickens.")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
