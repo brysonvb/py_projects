313a7bda4ae3c42838a9187b2c90148f2374d3f2
Bryson Bisuna <bryson@bisuna.com>
2022-01-31 18:12:43 -0800

    added migrations of models

--- web/django/diary/entries/views.py
+++ web/django/diary/entries/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
