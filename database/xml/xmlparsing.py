6e8d32cf51b78243f21ffa2b55af6bfc2ab3d355
Bernard Bisuna <bernard_bisuna@trendmicro.com>
2022-02-13 11:09:03 -0800

    xml parsing study

--- database/xml/xmlparsing.py
+++ database/xml/xmlparsing.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-from xml.dom.minidom import parse, parseString
+from xml.dom.minidom import parse, parseString, Node
+import xml.etree.ElementTree as ET
 
 fn = "smiley.svg"
+
+def set_id_attribute(parent, attribute_name = "id"):
+    if parent.nodeType == Node.ELEMENT_NODE:
+        if parent.hasAttribute(attribute_name):
+            parent.setIdAttribute(attribute_name)
+    for child in parent.childNodes:
+        set_id_attribute(child, attribute_name)
 
 def parsexml():
     document = parse(fn)
@@ -15,7 +23,15 @@ def parsexml():
 
 
 def main():
-    parsexml()
+    document = parse(fn)
+    set_id_attribute(document)
+    print(document.getElementById("skin"))
+    print(document.getElementsByTagName("ellipse"))
+    tree = ET.parse(fn)
+    root = tree.getroot()
+    #ET.dump(tree)
+    for elm in root.findall(""):
+        print(elm.attrib)
 
 
 if __name__ == '__main__':
